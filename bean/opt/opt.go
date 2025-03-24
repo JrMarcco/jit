ba01fb1cd83cf8b27f7fe0f65196c0355311e6ae
jrmarcco <jrmarcco@gmail.com>
2025-03-24 19:03:08 +0800

    feat: add opt method.

---
+++ bean/opt/opt.go
@@ -0,0 +1,24 @@
+package opt
+
+// Opt is generalized design for option pattern.
+type Opt[T any] func(*T)
+
+// Apply applies the options to the target.
+func Apply[T any](t *T, opts ...Opt[T]) {
+	for _, opt := range opts {
+		opt(t)
+	}
+}
+
+// OptErr is a variant of Opt that returns an error.
+type OptErr[T any] func(*T) error
+
+// ApplyErr applies the options to the target and returns an error if any option returns an error.
+func ApplyErr[T any](t *T, opts ...OptErr[T]) error {
+	for _, opt := range opts {
+		if err := opt(t); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ bean/opt/opt_test.go
@@ -0,0 +1,56 @@
+package opt
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type example struct {
+	Int int
+	Str string
+}
+
+func TestApply(t *testing.T) {
+	e := &example{}
+	Apply(e, func(e *example) {
+		e.Int = 1
+		e.Str = "test"
+	})
+
+	assert.Equal(t, e.Int, 1)
+	assert.Equal(t, e.Str, "test")
+}
+
+func withStrErr(str string) OptErr[example] {
+	return func(e *example) error {
+		if str == "" {
+			return errors.New("str error")
+		}
+		e.Str = str
+		return nil
+	}
+}
+
+func TestApplyErr(t *testing.T) {
+	e := &example{}
+	err := ApplyErr(e, func(e *example) error {
+		e.Int = 1
+		e.Str = "test"
+		return nil
+	})
+	require.NoError(t, err)
+	assert.Equal(t, e.Int, 1)
+	assert.Equal(t, e.Str, "test")
+
+	err = ApplyErr(e, withStrErr("test"))
+	require.NoError(t, err)
+	assert.Equal(t, e.Str, "test")
+
+	err = ApplyErr(e, withStrErr(""))
+	assert.Error(t, err, errors.New("str error"))
+	assert.Equal(t, e.Int, 1)
+	assert.Equal(t, e.Str, "test")
+}
