91fdfd73880329f3a0a70367869781fec206e92b
jrmarcco <jrmarcco@gmail.com>
2025-04-02 21:50:19 +0800

    feat: add map set.

---
+++ set/map_set_test.go
@@ -0,0 +1,122 @@
+package set
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// overcapacity
+func TestMapSet_Add(t *testing.T) {
+	tcs := []struct {
+		name    string
+		size    int
+		keys    []int
+		wantRes map[int]struct{}
+	}{
+		{
+			name:    "add vals",
+			size:    10,
+			keys:    []int{1, 2, 3, 4},
+			wantRes: map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			s := NewMapSet[int](tc.size)
+			for _, key := range tc.keys {
+				s.Add(key)
+			}
+
+			assert.Equal(t, tc.wantRes, s.m)
+		})
+	}
+}
+
+func TestMapSet_Del(t *testing.T) {
+	tcs := []struct {
+		name    string
+		delVal  int
+		oriSet  map[int]struct{}
+		wantSet map[int]struct{}
+	}{
+		{
+			name:    "del val",
+			delVal:  1,
+			oriSet:  map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+			wantSet: map[int]struct{}{2: {}, 3: {}, 4: {}},
+		}, {
+			name:    "del val not exist",
+			delVal:  5,
+			oriSet:  map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+			wantSet: map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			s := NewMapSet[int](len(tc.oriSet))
+			s.m = tc.oriSet
+			s.Del(tc.delVal)
+			assert.Equal(t, tc.wantSet, s.m)
+		})
+	}
+}
+
+func TestMapSet_Exist(t *testing.T) {
+	tcs := []struct {
+		name    string
+		val     int
+		oriSet  map[int]struct{}
+		wantRes bool
+	}{
+		{
+			name:    "exist val",
+			val:     1,
+			oriSet:  map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+			wantRes: true,
+		}, {
+			name:    "exist val not exist",
+			val:     5,
+			oriSet:  map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+			wantRes: false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			s := NewMapSet[int](len(tc.oriSet))
+			s.m = tc.oriSet
+			assert.Equal(t, tc.wantRes, s.Exist(tc.val))
+		})
+	}
+}
+
+func TestMapSet_Keys(t *testing.T) {
+	tcs := []struct {
+		name    string
+		oriSet  map[int]struct{}
+		wantRes []int
+	}{
+		{
+			name:    "keys",
+			oriSet:  map[int]struct{}{1: {}, 2: {}, 3: {}, 4: {}},
+			wantRes: []int{1, 2, 3, 4},
+		}, {
+			name:    "disorder keys",
+			oriSet:  map[int]struct{}{4: {}, 3: {}, 2: {}, 1: {}},
+			wantRes: []int{1, 2, 3, 4},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			s := NewMapSet[int](len(tc.oriSet))
+			s.m = tc.oriSet
+			keys := s.Keys()
+
+			assert.ElementsMatch(t, tc.wantRes, keys)
+		})
+	}
+}
